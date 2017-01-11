4b6a87933fc0f4fedd7bf39af87a18a5b86905ba
Hyun Kim <khyun0428@gmail.com>
2017-01-11 07:37:16 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  
+  get 'blogs' => 'blogs#index'
+  
+  post 'blogs/create' => 'blogs#create'
+  
+  get 'blogs/new' => 'blogs#new'
+  
+  get 'posts/:id' => 'blogs#show'
+  
+  get 'posts/edit/:id' => 'blogs#edit'
+  
+  put 'posts/update' => 'blogs#update'
+  
+  get 'posts/delete/:id' => 'blogs#delete'
+      
   root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -8,7 +23,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+ 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
